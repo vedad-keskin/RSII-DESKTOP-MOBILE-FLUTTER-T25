b24c654c40a70815cae6f6bec9652df997b886c5
Vedad Keskin <vedad.keskin98@gmail.com>
2025-12-11 08:38:36 +0100

    Cart Events Form Frontend

--- Workshops/3. ISPITNI 17-09-2025 TEST/eCommerce/UI/ecommerce_mobile/lib/layouts/master_screen.dart
+++ Workshops/3. ISPITNI 17-09-2025 TEST/eCommerce/UI/ecommerce_mobile/lib/layouts/master_screen.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_mobile/screens/cart_screen.dart';
+import 'package:ecommerce_mobile/screens/frmCartEventsIB180079.dart';
 import 'package:ecommerce_mobile/screens/product_details_screen.dart';
 import 'package:ecommerce_mobile/screens/product_list.dart';
 import 'package:flutter/material.dart';
@@ -33,6 +34,9 @@ class _MasterScreenState extends State<MasterScreen> {
               Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProductDetailsScreen()));
             },), ListTile(title: Text('Cart'), onTap: () {
               Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => CartScreen()));
+            },),
+            ListTile(title: Text('Cart Events'), onTap: () {
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => FrmCartEventsIB180079()));
             },),
           ],
         ),
--- Workshops/3. ISPITNI 17-09-2025 TEST/eCommerce/UI/ecommerce_mobile/lib/main.dart
+++ Workshops/3. ISPITNI 17-09-2025 TEST/eCommerce/UI/ecommerce_mobile/lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:ecommerce_mobile/model/cart_event_provider.dart';
 import 'package:ecommerce_mobile/model/cart_provider.dart';
 import 'package:ecommerce_mobile/providers/auth_provider.dart';
 import 'package:ecommerce_mobile/providers/logged_product_provider.dart';
 import 'package:ecommerce_mobile/providers/product_provider.dart';
 import 'package:ecommerce_mobile/providers/product_type_provider.dart';
 import 'package:ecommerce_mobile/providers/unit_of_measure_provider.dart';
+import 'package:ecommerce_mobile/screens/frmCartEventsIB180079.dart';
 import 'package:ecommerce_mobile/screens/product_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,6 +19,7 @@ void main() {
     ChangeNotifierProvider<ProductTypeProvider>(
         create: (context) => ProductTypeProvider()),
     ChangeNotifierProvider<CartProvider>(create: (context) => CartProvider()),
+    ChangeNotifierProvider<CartEventProvider>(create: (context) => CartEventProvider()),
   ], child: const MyLoginApp()));
 }
 
